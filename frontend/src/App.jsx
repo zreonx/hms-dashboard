c34b36ac3c5cae489e63b3dd715daa62bd71ec36
Zreon <56314835+zreonx@users.noreply.github.com>
2024-05-07 14:19:18 +0800

    added first row layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,25 @@ import PatientStatisticsChart from "./components/PatientStatisticsChart";
 
 const App = () => {
   return (
-    <>
-      {/* <div className='flex flex-row gap-5 p-5 max-md:flex-col'>
+    <div className='bg-default h-screen py-4 px-3'>
+      <h1 className='text-2xl font-semibold text-slate-700'>
+        Welcome to MMGG!
+      </h1>
+      <h3 className='text-sm text-slate-500 mt-1'>Hospital Admin Dashboard</h3>
+      <div className='flex flex-col gap-5 py-5 md:flex-row'>
         <CardTotals />
         <CardTotals />
         <CardTotals />
         <CardTotals />
-      </div> */}
-
-        <div className=" py-5 px-3 flex flex-col justify-center align-center">
-        <PatientStatisticsChart/>
-        
-        <div>Pie chart</div>
       </div>
-    </>
+
+      <div className='flex justify-center align-center w-auto gap-4 flex-col md:flex-row'>
+        <PatientStatisticsChart />
+        <div className=' border rounded-lg  bg-white p-5'>
+          <h1>Pie chart</h1>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- frontend/src/components/CardTotals.jsx
+++ frontend/src/components/CardTotals.jsx
@@ -1,6 +1,6 @@
 const CardTotals = () => {
   return (
-    <div className='p-5 border shadow-sm rounded-md'>
+    <div className='p-5 border shadow-sm rounded-md bg-white w-100 md:w-[20rem]'>
       <h3>Total Patients</h3>
       <img src='' alt='' />
       <span>255</span>
